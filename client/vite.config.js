dbdf2d9a8032599b7239cfe7a93ab0136aa41c0f
tom2811 <oaksoe.thein01@gmail.com>
2024-10-15 02:37:43 +0630

    First commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        './runtimeConfig': './runtimeConfig.browser',
-      },
-    },
-    optimizeDeps: {
-      include: ['firebase/app', 'firebase/auth']
-    },
-    define: {
-      'process.env': process.env
-    },
-    build: {
-      sourcemap: mode === 'development'
-    },
-    server: {
-      open: true,
-      port: 3000,
-      proxy: {
-        '/api': 'http://localhost:5000'
-      }
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
   }
 })
