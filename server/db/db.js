5a98ac80b5a2ca699962c2971b1a8363e3e110f9
tom2811 <oaksoe.thein01@gmail.com>
2024-10-19 20:51:40 +0630

    Fix MongoDB and Vite errors

--- server/db/db.js
+++ server/db/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       dbName: 'kickskraze'
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
